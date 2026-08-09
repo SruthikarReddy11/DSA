769d9f63798116b0f60766a1507623a944917f75
SruthikarReddy11 <sruthikarreddy11@gmail.com>
2026-08-09 13:02:13 +0530

    Create Bubble_Sort.java

---
+++ Sorting_Algorithms/Bubble_Sort.java
@@ -0,0 +1,29 @@
+public class Binary{
+  public static void main(String args[]){
+   int a[] = {8,6,9,2,4,5};
+   int size = a.length;
+   int temp = 0;
+   System.out.println("Elements before sorting:");
+   for(int num : a){
+       System.out.print(num + " ");
+   }
+   for(int i=0;i<size;i++){
+       for(int j=0;j<size-i-1;j++){ //We are using j<size-i-1 , because we dont want to repeat the sorted iterations.
+           if(a[j] > a[j+1]){
+               temp = a[j];
+               a[j] = a[j+1];
+               a[j+1] = temp;
+           }
+       }
+       System.out.println();
+       for(int num : a){
+       System.out.print(num + " ");
+   }
+   }
+   System.out.println();
+   System.out.println("Elements after sorting:");
+   for(int num : a){
+       System.out.print(num + " ");
+   }
+  }
+}
