3f5150737cd619edbc783bc7c4b58ee69bbd47cc
SruthikarReddy11 <sruthikarreddy11@gmail.com>
2026-08-25 21:01:40 +0530

    Create frequency_of_digit.py

---
+++ Python_basic_questions/frequency_of_digit.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr = list(map(int,input().split()))
+
+freq = {}
+
+for i in arr:
+    if i in freq:
+        freq[i] += 1
+    else:
+        freq[i] = 1
+for i in freq:
+    print(i , ":",freq[i])
